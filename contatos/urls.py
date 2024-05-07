fd72bd9f6343538886038670b4e2efdf4ec5099e
a92212126 <alexwallax@gmail.com>
2024-05-07 16:51:03 -0300

    Primeiro commit

---
+++ contatos/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from contatos.views import index, contato
+
+urlpatterns = [
+    path('', index),
+    path('contato', contato, name='contato')
+]
+
---
+++ contatos/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.urls import path
+# Create your views here.
+
+def index(request):
+    return render(request, 'contatos/index.html')
+
+def contato(request):
+    return render(request, 'contatos/det_contato.html')
