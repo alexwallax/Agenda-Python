061ce102c1bfdeba511e51997886d2c9daf23134
a92212126 <alexwallax@gmail.com>
2024-05-14 16:00:19 -0300

    persistencia02

--- contatos/views.py
+++ contatos/views.py
@@ -1,22 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.urls import path
+from contatos.models import Contato
 # Create your views here.
 
 def index(request):
-    dados = {
-    1: {'nome': 'João Grilo',
-        'telefone': '(83) 992343211'},
-    2: {'nome': 'Chicó',
-        'telefone': '(83) 992343212'},
-    3: {'nome': 'Rosinha',
-        'telefone': '(83) 992343213'},
-    4: {'nome': 'Padeiro',
-        'telefone': '(83) 992343214'},
-    5: {'nome': 'Dora',
-        'telefone': '(83) 992343215'}
-}
-    return render(request, 'contatos/index.html',  {"conts": dados})
+    contatos = Contato.objects.all()
+    return render(request, 'contatos/index.html',  {'cont': contatos})
 
 def contato(request):
     return render(request, 'contatos/det_contato.html')
