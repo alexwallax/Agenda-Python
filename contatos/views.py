a8effb44fca57774cd78fc2dfe67ebdcb8f08fbf
a92212126 <alexwallax@gmail.com>
2024-05-14 15:25:05 -0300

    persistência

--- contatos/views.py
+++ contatos/views.py
@@ -4,7 +4,19 @@ from django.urls import path
 # Create your views here.
 
 def index(request):
-    return render(request, 'contatos/index.html')
+    dados = {
+    1: {'nome': 'João Grilo',
+        'telefone': '(83) 992343211'},
+    2: {'nome': 'Chicó',
+        'telefone': '(83) 992343212'},
+    3: {'nome': 'Rosinha',
+        'telefone': '(83) 992343213'},
+    4: {'nome': 'Padeiro',
+        'telefone': '(83) 992343214'},
+    5: {'nome': 'Dora',
+        'telefone': '(83) 992343215'}
+}
+    return render(request, 'contatos/index.html',  {"conts": dados})
 
 def contato(request):
     return render(request, 'contatos/det_contato.html')
